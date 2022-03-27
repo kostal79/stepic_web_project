7d5a711fbf16c93b4dd58a8368f49e1ec7ea6638
Alex <74615724+kostal79@users.noreply.github.com>
2022-03-27 20:36:10 +0300

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo ﻿ln -s /home/alex/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/alex/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+﻿sudo /etc/init.d/mysql start﻿
